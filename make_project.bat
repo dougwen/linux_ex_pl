70df36e1ef162bd76ae5e731bcf9fda7b3dadc7b
Wenstrand <douglas.wenstrand@jhuapl.edu>
2024-08-21 16:17:00 -0400

    update to 2023.2 and no software build

--- make_project.bat
+++ make_project.bat
@@ -1,7 +1,7 @@
-call c:\Xilinx\Vivado\2022.1\settings64.bat
+call c:\Xilinx\Vivado\2023.2\settings64.bat
 call vivado -nolog -nojournal -mode=batch -source=./tcl/make_project.tcl
 call vivado -nolog -nojournal -mode=batch -source=./tcl/impl.tcl
-call xsct tcl/make_sw.tcl
-call vivado -nolog -nojournal -mode batch -source ./tcl/make_final_bitstream.tcl
+#call xsct tcl/make_sw.tcl
+#call vivado -nolog -nojournal -mode batch -source ./tcl/make_final_bitstream.tcl
 cd tcl
 call bootgen -image makebin.bif -arch zynq -process_bitstream bin -w on
